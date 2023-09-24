132c070c11ca3e0cb80adb7627d77ad80a6973d9
Flávio Roberto <124847034+FlavioRobertoTavares@users.noreply.github.com>
2023-09-24 11:54:03 -0300

    Delete P3/nivel 1/nivel 2/Controle do magnetron/nivel 3/AND OR NOT lógico directory

--- P3/nivel 1/nivel 2/Controle do magnetron/nivel 3/AND OR NOT lógico/AND OR NOT lógico.v
+++
@@ -1,7 +0,0 @@
-module AND_OR_NOT_logic (input wire starn, stopn, clearn, door_closed, timer_done,
-                        output reg set, reset);
-                        
-    assign set = !starn && door_closed && !timer_done;
-    assign reset = !stopn | !clearn | !door_closed | timer_done;
-
-endmodule
