17076620973196a411707633af7c4145d5762f4e
Gabriel Ayres <127794150+GabrielCFAyres@users.noreply.github.com>
2023-09-24 10:45:11 -0300

    Update non_recycling_counter.v

--- P3/nivel 1/nivel 2/Entrada de timer e controle/Nível 3/Contador não reciclável/non_recycling_counter.v
+++ P3/nivel 1/nivel 2/Entrada de timer e controle/Nível 3/Contador não reciclável/non_recycling_counter.v
@@ -5,22 +5,16 @@ module non_recycling_counter(
 );
 
     reg [2:0] counter;
-    always @ (posedge clock or posedge clear)
-        begin
-            if (clear)
-                begin
-                    counter = 3'b000;
-                    non_recycling_counter_output = 0;
-                end
-            else
-                if (counter == 3)
-                    begin
-                        non_recycling_counter_output = 1;
-                        counter = counter + 1'b1;
-                    end
-                else if (counter < 7)
-                    begin
-                        counter = counter + 1'b1;
-                    end
+
+    always @(posedge clock or posedge clear) begin
+        if (clear) begin
+            counter <= 3'b000;
+          	non_recycling_counter_output <= 0;
+        end else if (counter == 3) begin
+                counter <= counter + 1'b1;
+                non_recycling_counter_output <= 1;
+            end else if (counter < 7) begin
+                counter <= counter + 1'b1;
+            end
         end
 endmodule
